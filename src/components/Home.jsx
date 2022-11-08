a0230a72983216b94edd453a573c2078ee217343
themodernmonk7 <sauravavishek@gmail.com>
2022-11-08 18:50:53 +0530

    Refactor design

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,91 +1,101 @@
-import React from 'react'
-import { HiArrowLongRight } from "react-icons/hi2"
-import SauravImage from "../assets/SauravBW.jpg"
-import { AiOutlineTwitter, AiOutlineGithub } from "react-icons/ai"
-import { FaLinkedinIn } from "react-icons/fa"
+import { Link } from "react-router-dom"
+import { Person } from "../data"
 
 const Home = () => {
   return (
     <>
-      <section className=" container mx-auto flex flex-col-reverse md:flex-row justify-evenly md:justify-between lg:px-44 items-center  h-screen relative px-8 ">
-        {/* Left  */}
-        <div className=" flex flex-col justify-start max-w-xl ">
+      <section className=" container mx-auto flex flex-col-reverse xl:flex-row md:justify-between justify-evenly items-center h-screen   relative md:my-32 md:px-20 mt-10 ">
+        {/* justify-around md:justify-between items-center */}
+        {/* Left text  */}
+        <div className=" flex flex-col justify-start w-full xl:w-1/2 px-5 md:px-0  ">
           <div className="  space-y-6 ">
-            <h1 className=" text-3xl  md:text-5xl lg:text-6xl lg:leading-[80px] font-semibold lowercase ">
-              Creative designer & a developer.
+            <h1 className=" text-[30px] text-ellipsis text-left  md:text-5xl lg:text-6xl lg:leading-[80px] font-semibold lowercase ">
+              {Person.tagline_text}
             </h1>
-            <h2 className=" md:text-lg text-gray-400 font-medium ">
-              Hi I'm SAurav, a passionate Front-end Developer & UI/UX Designer
-              based in the India.{" "}
-            </h2>
+            <h2 className=" md:text-lg text-gray-400 ">{Person.short_bio}</h2>
           </div>
 
-          <div className="flex flex-col items-start space-y-10 mt-10 md:mt-20  ">
-            <div className="space-x-2 relative hover:bg-blue-600  rounded-full py-1 flex  items-center">
-              <a href="#" className="uppercase font-medium ">
-                See my works
-              </a>
-              <div className="md:w-12 md:h-12 h-8 w-8 bg-blue-600 rounded-full absolute -left-6 -z-10 "></div>
-              <span>
-                {" "}
-                <HiArrowLongRight className="w-10 h-10" />{" "}
+          <div className="flex flex-col items-start space-y-10  ">
+            <Link
+              to="/works"
+              className="ease-[cubic-bezier(.77, 0, 0.175, 1)] relative mt-12  flex  w-[210px] transform cursor-pointer  items-center space-x-2 py-1 text-lg font-semibold transition-all duration-500 hover:translate-x-4 md:mt-32 md:w-[230px] md:font-bold"
+            >
+              <span className=" before:ease-[cubic-bezier(.77, 0, 0.175, 1)] py-2 before:absolute before:-left-5 before:bottom-[6px] before:-z-10 before:block before:h-10 before:w-10 before:rounded-full before:bg-gray-200 before:transition-all before:duration-500 before:hover:w-full md:before:-left-6 md:before:top-0 md:before:h-14 md:before:w-14   ">
+                view all works
+                <span className="absolute bottom-2 ml-2">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    strokeWidth={1}
+                    stroke="currentColor"
+                    className="h-8 w-8 "
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="M17.25 8.25L21 12m0 0l-3.75 3.75M21 12H1"
+                    />
+                  </svg>
+                </span>
               </span>
-            </div>
+            </Link>
           </div>
         </div>
-        {/* Right */}
+        {/* Right image */}
         <div className=" relative flex justify-center items-center ">
           <img
-            src={SauravImage}
+            src={Person.image[1]}
             alt=""
-            className=" w-44 h-44 md:w-80 md:h-80 object-cover rounded-full "
+            className=" w-44 w h-44 md:w-80 md:h-80 object-cover rounded-full "
           />
-          <div className="w-44 h-44 md:w-full md:h-full rounded-full animate-pulse bg-blue-600 absolute blur-3xl -z-10 "></div>
+          <div className="w-52 h-52 md:w-full md:h-full rounded-full animate-pulse bg-blue-700 absolute blur-3xl -z-10 "></div>
 
           {/* small size cross for mobile cross */}
-          <span className="absolute md:-left-3 md:bottom-3 md:w-96 md:h-96 -z-10 md:hidden  ">
+          <span className="absolute md:-left-3 md:bottom-3 md:w-96 md:h-96 -z-10 md:hidden  px-5 py-5 ">
             <svg
-              width="180"
-              height="450"
+              width="200"
+              height="230"
               viewBox="0 0 122 137"
-              fill="none"
+              fill="red"
               xmlns="http://www.w3.org/2000/svg"
             >
-              <rect width="122" height="137" fill="noe" />
+              <rect width="122" height="137" fill="none" />
               <rect
                 x="106.06"
-                width="14"
+                width="17"
                 height="165"
                 transform="rotate(40 106.06 0)"
-                fill="white"
+                fill="black"
               />
               <rect
                 x="3"
                 y="10.2847"
-                width="14"
+                width="17"
                 height="165"
                 transform="rotate(-40 3 10.2847)"
-                fill="white"
+                fill="black"
               />
             </svg>
           </span>
 
           {/* Medium size for large screen cross */}
-          <span className="absolute -left-3 bottom-3 w-96 h-96 -z-10 md:hidden hidden  ">
+          <span className=" absolute  -z-10 hidden xl:grid  ">
+            {/* absolute -left-3 bottom-3 w-96 h-96 */}
             <svg
-              width="350"
+              width="450"
               height="450"
               viewBox="0 0 122 137"
               fill="none"
               xmlns="http://www.w3.org/2000/svg"
             >
-              <rect width="122" height="137" fill="noe" />
+              <rect width="122" height="137" fill="none" />
               <rect
                 x="106.06"
                 width="14"
                 height="165"
                 transform="rotate(40 106.06 0)"
-                fill="white"
+                fill="black"
               />
               <rect
                 x="3"
@@ -93,14 +103,14 @@ const Home = () => {
                 width="14"
                 height="165"
                 transform="rotate(-40 3 10.2847)"
-                fill="white"
+                fill="black"
               />
             </svg>
           </span>
         </div>
 
         {/* Social icons */}
-        <div className="flex hidden flex-col space-y-12 absolute -left-10  bottom-1/3 ">
+        {/* <div className="flex hidden flex-col space-y-12 absolute -left-10  bottom-1/3 ">
           <span className="hover:bg-gray-500 grid place-items-center px-2 py-2 rounded-full ">
             {" "}
             <AiOutlineTwitter className="w-6 h-6 " />{" "}
@@ -113,7 +123,7 @@ const Home = () => {
             {" "}
             <FaLinkedinIn className="w-6 h-6 " />{" "}
           </span>
-        </div>
+        </div> */}
       </section>
     </>
   )
